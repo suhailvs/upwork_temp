fbe8dac55ec832c8507b209e24032dc2b37cd2ab
Suhail vs <suhailvs@gmail.com>
2017-09-26 13:20:48 +0530

    Added twitter bootstrap themes

--- uploader/views.py
+++ uploader/views.py
@@ -4,14 +4,38 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from uploader.models import UploadForm,UserEmail
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.core.validators import validate_email
 # Create your views here.
+from django.core.mail import send_mail
+from django.core.exceptions import ValidationError
 def home(request):
 	if request.method=="POST":
-		usr= UserEmail(email=request.POST['email'])
-		usr.save()
-		return HttpResponseRedirect(reverse('home'))
+		try:
+			validate_email(request.POST.get("email", ""))
+		except ValidationError as e:
+			#print "oops! wrong email"
+			return HttpResponse('''<h1> Invalid email</h1>
+				Please provide xx@ss.xx format email. Thanks''')
+		else:
+			#print "hooray! email is valid"
+
+			usr= UserEmail(email=request.POST['email'])
+			usr.save()
+
+			email_dec = """
+			Please Visit http://suhailvs.mooo.com/user/{0}/ 
+			and Upload your profile picture.
+
+			Thanks
+			This is just a test
+			""".format(usr.pk)
+
+			send_mail('Please Upload your Image', 
+				email_dec,
+				'suhailvs@gmail.com',[usr.email],fail_silently=False)
+			return HttpResponseRedirect(reverse('home'))
 	
 	users=UserEmail.objects.all()
 	return render(request,'home.html',{'users':users})
@@ -29,11 +53,11 @@ def upload(request,pk):
 		if img.is_valid():
 			usr.pic = request.FILES['pic']
 			usr.save()	
-			print 'file saved'
+			#'file saved'
 			return HttpResponseRedirect(reverse('user_pic',args=[pk]))
 	else:
 		img=UploadForm()
-	return render(request,'upload.html',{'form':img})
+	return render(request,'upload.html',{'form':img,'user':usr})
 		
 	
 		
