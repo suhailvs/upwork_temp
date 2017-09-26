4e2494923acdfb5fedf562c027ba422b25f83b15
Suhail vs <suhailvs@gmail.com>
2017-09-26 11:46:35 +0530

    added file upload for specific user

--- uploader/views.py
+++ uploader/views.py
@@ -2,17 +2,38 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from uploader.models import UploadForm,Upload
+from uploader.models import UploadForm,UserEmail
+
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 # Create your views here.
 def home(request):
 	if request.method=="POST":
-		img = UploadForm(request.POST, request.FILES)		
+		usr= UserEmail(email=request.POST['email'])
+		usr.save()
+		return HttpResponseRedirect(reverse('home'))
+	
+	users=UserEmail.objects.all()
+	return render(request,'home.html',{'users':users})
+
+def upload(request,pk):
+	usr=UserEmail.objects.get(pk=pk)
+	if usr.pic:
+		# if pic exist. show the pic
+		return render(request,'view.html',{'user':usr})
+
+	# if pic doesnt exist. show the fileupload
+	if request.method=="POST":
+		# if post create the pic
+		img = UploadForm(request.FILES)		
 		if img.is_valid():
-			img.save()	
-			return HttpResponseRedirect(reverse('imageupload'))
+			usr.pic = request.FILES['pic']
+			usr.save()	
+			print 'file saved'
+			return HttpResponseRedirect(reverse('user_pic',args=[pk]))
 	else:
 		img=UploadForm()
-	images=Upload.objects.all()
-	return render(request,'home.html',{'form':img,'images':images})
+	return render(request,'upload.html',{'form':img})
+		
+	
+		
