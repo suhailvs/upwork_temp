a938b2a2550eb8eac449fb1f482295db3d47cd5e
Suhail vs <suhailvs@gmail.com>
2017-10-06 21:36:49 +0530

    added a model Synclog for syncronization logs

--- uploader/models.py
+++ uploader/models.py
@@ -9,7 +9,7 @@ class User(models.Model):
 	firstname=models.CharField(max_length=50)
 	lastname=models.CharField(max_length=50)
 	pic = models.FileField(upload_to="images/",blank=True,null=True)    
-	upload_date=models.DateTimeField(auto_now_add =True)
+	date=models.DateTimeField(auto_now_add =True)
 
 class Candidate(models.Model):
 	cId=models.CharField(max_length=10)
@@ -18,10 +18,14 @@ class Candidate(models.Model):
 	lastname=models.CharField(max_length=50)
 	recruiter = models.CharField(max_length=10)
 	employer = models.CharField(max_length=50)
-	email_sent = models.DateTimeField(auto_now_add =True)
+	date = models.DateTimeField(auto_now_add =True)
 
 # FileUpload form class.
 class UploadForm(ModelForm):
 	class Meta:
 		model = User		
 		fields = ('pic',)
+
+class Synclog(models.Model):
+	status = models.CharField(max_length=20,default='fail')
+	date = models.DateTimeField(auto_now_add =True)
