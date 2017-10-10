c9a6c3287538b76e6572f9d14f6eb445464f6795
Suhail vs <suhailvs@gmail.com>
2017-10-10 16:54:57 +0530

    This closes #6, closes #4, closes #5 - To solve issue 5 I commented out send email function. we can uncomment it later after the full sync.

--- uploader/admin.py
+++ uploader/admin.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import User,Candidate,Synclog
+
+admin.site.register(User)
+admin.site.register(Candidate)
+admin.site.register(Synclog)
--- uploader/tasks.py
+++ uploader/tasks.py
@@ -10,14 +10,7 @@ def api_request(endpoint):
 	api_url="http://api.jobadder.com/v2"+endpoint
 	req = urllib2.Request(api_url)
 	req.add_header('Authorization',api_key)
-	resp=urllib2.urlopen(req)
-	"""
-	try:
-		resp=urllib2.urlopen(req)
-	except urllib2.HTTPError, err:
-		print 'Http ERROR:'+str(err.code)
-		raise
-	"""
+	resp=urllib2.urlopen(req)	
 	return json.loads(resp.read())
 
 def sync_users():
@@ -27,13 +20,16 @@ def sync_users():
 		usr= User(uId=u['userId'],firstname=u['firstName'],
 			lastname=u['lastName'],email=u['email'])
 		usr.save()
-		email_user(usr)
+		#email_user(usr)
 
 def sync_candidates():
 	data = api_request('/candidates')
 	for c in data['items']:
 		c_id=c['candidateId']
+		status = c['status']['name']
+		if status not in ['Placed (PERM)','Placed (CONTRACT)']: continue
 		if Candidate.objects.filter(cId=c_id):continue
+		
 		d2 = api_request('/candidates/'+str(c_id))
 		cand = Candidate(cId=c['candidateId'],firstname=c['firstName'],
 			lastname=c['lastName'],email=c['email'],
@@ -41,7 +37,7 @@ def sync_candidates():
 		if 'employment' in d2:
 			cand.employer=d2["employment"]['current']['employer']
 		cand.save()
-		email_candidate(cand)		
+		#email_candidate(cand)		
 	
 @shared_task
 def sync_api():
